68c0a975e44ec51816e36adfb2b4c2422fdd21ad
nicohr3366 <nhernandezr@correo.usbcali.edu.co>
2026-04-10 12:56:32 -0500

    style: agrega estilos y mejoras de funcionamiento al portal

--- wayne_enterprise/urls.py
+++ wayne_enterprise/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('foundation/', include('foundation.urls')),
+    path('realestate/', include('realestate.urls')),
 ]
