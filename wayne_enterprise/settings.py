163f210f12bb254404a58af282bf73ed8c115cfc
Cuervo <felipecuervo0515@gmail.com>
2026-05-09 13:16:32 -0500

    feat: agrega base.html compartido con navbar y registra app tech

--- wayne_enterprise/settings.py
+++ wayne_enterprise/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'foundation',
     'accounts',
     'realestate',
+    'tech',                  # ← Wayne Technologies (Cuervo / Diego)
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'wayne_enterprise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # ← carpeta templates/ compartida
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
